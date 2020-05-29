200f3cd95c112d9a17f19f07fcef966715fa3086
Tanaf <tanaf.dev@gmail.com>
2020-05-29 14:46:41 +0600

    Service Section

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.toggle-button').click(function(){
+       $('.header-hide').slideToggle(2000); 
+    });
+    var typed = new Typed('#type',{
+        strings: [
+            '',
+            'Web Designer'
+        ],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+    });
+    var typed = new Typed('#type-1',{
+        strings: [
+            '',
+            'Web Designer'
+        ],
+        loop: true,
+        typeSpeed: 80,
+        backSpeed: 80,
+    });
+});
+
